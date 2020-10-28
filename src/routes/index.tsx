a66b320e6dc3e6a1bf1d997e0204ff932e1206a1
Thiago Pereira <thiagorpereira7@gmail.com>
2020-10-28 11:18:56 -0300

    Inserindo cores default e estrutura das pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 import Home from '../pages/Home';
-import Cart from '../pages/Cart';
+import DebitConsultation from '../pages/DebitConsultation';
+import Proof from '../pages/Proof';
+import EditProfile from '../pages/EditProfile';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
-    <Route path="/cart" component={Cart} />
+    <Route path="/debit_consultation" component={DebitConsultation} isPrivate />
+    <Route path="/proof" component={Proof} isPrivate />
+    <Route path="/edit_profile" component={EditProfile} isPrivate />
+    {/* <Route path="/home" component={Home} isPrivate /> */}
   </Switch>
 );
 
---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#5D64D3',
+  secondary: '#E5E5E5',
+  tertiary: '#34BEAE',
+  grayDark: '#4B4E5C',
+  grayLight: '#A1A5BC',
+  fontPrimary: '#F5F7FE',
+};
+
+export default colors;
