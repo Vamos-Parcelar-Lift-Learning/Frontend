d948d84d3608cae9f4e7763e8cf0539e18d9420d
Thiago Pereira <thiagorpereira7@gmail.com>
2020-10-28 11:26:30 -0300

    Sobrescrevendo Route p/ utilizar o Layout Default

---
+++ src/pages/EditProfile/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const EditProfile: React.FC = () => {
+  return <h1> Editar Perfil </h1>;
+};
+
+export default EditProfile;
---
+++ src/pages/Proof/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Proof: React.FC = () => {
+  return <h1>Comprovantes</h1>;
+};
+
+export default Proof;
