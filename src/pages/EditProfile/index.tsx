dd55b37605aba46b6c43b170f6a7d3d8edc0e236
Kamilla Costa Souza <kamillacostasouza@hotmail.com>
2020-10-31 22:21:24 -0300

    Adicionando containers dos campos

--- src/pages/EditProfile/index.tsx
+++ src/pages/EditProfile/index.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import { Button } from '../../components';
 import {
-  Container,
   Title1,
   Title2,
   Title3,
+  Container,
   SubContainer,
-  ContainerLeft
+  ContainerName,
+  ContainerData,
+  ContainerCPF
 } from './styles';
 import user from '../../images/user.png';
 
 const EditProfile: React.FC = () => {
   return (
-    <SubContainer>
-      <Container>
-        <Title1>Editar Perfil</Title1>
-        <ContainerLeft>
-          <img src={user} alt="Alterar Foto de Perfil" />
-          <Title2>Alterar Foto</Title2>
-        </ContainerLeft>
-      </Container>
+    <Container>
+      <Title1>Editar Perfil</Title1>
+      <SubContainer>
+        <img src={user} alt="Alterar Foto de Perfil" />
+        <Title2>Alterar Foto</Title2>
+      </SubContainer>
+      <ContainerName>
+        <Title3>Nome:</Title3>
+      </ContainerName>
+      <ContainerData>
+        <Title3>Data de Nascimento:</Title3>
+      </ContainerData>
+      <ContainerCPF>
+        <Title3>CPF:</Title3>
+      </ContainerCPF>
       <Button>Salvar</Button>
-    </SubContainer>
+    </Container>
   );
 };
 
--- src/pages/EditProfile/styles.ts
+++ src/pages/EditProfile/styles.ts
@@ -1,35 +1,60 @@
 import styled from 'styled-components';
-import { makeStyles } from '@material-ui/core/styles';
 
 import colors from '../../styles/colors';
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  align-items: Center;
-`;
+// export const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   flex: 1;
+//   align-items: Center;
+// `;
 
-export const SubContainer = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
   align-items: center;
 `;
 
-export const ContainerLeft = styled.div`
+export const SubContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-right: 800px;
+  margin-right: 850px;
   align-items: left;
-  flex: 0.2;
+  flex: 0.3;
 `;
-export const Form = styled.div`
+
+export const ContainerName = styled.div`
+  width: 100px;
+  padding-left: 3px;
   display: flex;
   flex-direction: column;
-  margin-right: 800px;
+  margin-right: 850px;
   align-items: left;
+  flex: 0.12;
 `;
+
+export const ContainerData = styled.div`
+  display: grid;
+  width: 100px;
+  padding-left: 3px;
+  flex-direction: column;
+  margin-right: 850px;
+  align-items: left;
+  white-space: nowrap;
+  flex: 0.15;
+`;
+
+export const ContainerCPF = styled.div`
+  width: 100px;
+  padding-left: 3px;
+  display: flex;
+  flex-direction: column;
+  margin-right: 850px;
+  align-items: left;
+  flex: 0.1;
+`;
+
 export const Title2 = styled.span`
   width: 100%;
   font-size: 12px;
