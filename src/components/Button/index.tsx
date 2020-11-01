d5a498ec2a9779af3af9763178f3668952963795
mrodrigues <matheus.1nascimento58@gmail.com>
2020-11-01 12:54:26 -0300

    adicionado componente Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 
-import { ButtonContainer } from './styles';
+import { ButtonContainer, useStyles, Loading } from './styles';
 
 interface ButtonProps {
-  childre?: any;
+  children: any;
+  loading?: boolean;
+  style: any;
 }
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
-  return <ButtonContainer>{children}</ButtonContainer>;
+const Button: React.FC<ButtonProps> = ({
+  children,
+  loading = false,
+  style,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <ButtonContainer
+      style={style}
+      classes={{
+        root: classes.button,
+        label: classes.label,
+      }}
+    >
+      {loading ? <Loading style={{ width: 25, height: 25 }} /> : children}
+    </ButtonContainer>
+  );
 };
 
 export default Button;
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Button, CircularProgress } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import colors from '../../styles/colors';
 
 export const Container = styled.div``;
@@ -7,9 +8,26 @@ export const Container = styled.div``;
 export const ButtonContainer = styled(Button)`
   && {
     color: ${colors.fontPrimary};
-    background-color: ${colors.primary};
+    background-color: ${colors.PRIMARY};
     font-size: 14px;
     padding: 10px 100px;
-    border-radius: 20px;
+    border-radius: 25px;
+    height: 48px;
+  }
+`;
+
+export const useStyles = makeStyles({
+  button: {
+    background: `linear-gradient(45deg, ${colors.TERTIARY} 5%, ${colors.PRIMARY} 90%)`,
+  },
+  label: {
+    textTransform: 'capitalize',
+    fontSize: 16,
+  },
+});
+
+export const Loading = styled(CircularProgress)`
+  && {
+    color: white;
   }
 `;
