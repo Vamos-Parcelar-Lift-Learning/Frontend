125cbe462fecde210be87bd824a3145ee4a5264d
Thiago Pereira <thiagorpereira7@gmail.com>
2020-11-04 15:17:57 -0300

    Configurações finais

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,16 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 import Home from '../pages/Home';
 import DebitConsultation from '../pages/DebitConsultation';
-import Proof from '../pages/Proof';
+import ReceiptList from '../pages/ReceiptList';
+import Receipt from '../pages/Receipt';
 import EditProfile from '../pages/EditProfile';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/debit_consultation" component={DebitConsultation} isPrivate />
-    <Route path="/proof" component={Proof} isPrivate />
+    <Route path="/receipt" component={ReceiptList} isPrivate />
+    <Route path="/receiptDetail" component={Receipt} isPrivate />
     <Route path="/edit_profile" component={EditProfile} isPrivate />
     {/* <Route path="/home" component={Home} isPrivate /> */}
   </Switch>
