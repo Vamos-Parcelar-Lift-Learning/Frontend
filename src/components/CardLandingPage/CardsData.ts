14f75991b670f160df2ed38ca6645af36284b653
Kamilla Costa Souza <kamillacostasouza@hotmail.com>
2020-11-12 21:08:34 -0300

    Removendo yarn.lock

--- src/components/CardLandingPage/CardsData.ts
+++ src/components/CardLandingPage/CardsData.ts
@@ -1,19 +1,20 @@
+import { useTranslation } from 'react-i18next';
 import { Icons } from '../../assets';
 
 export const CARD_DATA = [
   {
     id: 1,
     title: 'Escolha o que você quer pagar',
-    img: Icons.ICON_DEBIT_CHOICE,
+    img: Icons.ICON_DEBIT_CHOICE
   },
   {
     id: 2,
     title: 'Pague com o Pix',
-    img: Icons.ICON_QRCODE,
+    img: Icons.ICON_QRCODE
   },
   {
     id: 3,
     title: 'Receba Cashback',
-    img: Icons.ICON_CASHBACK,
-  },
+    img: Icons.ICON_CASHBACK
+  }
 ];
