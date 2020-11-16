e65bd66c548bc5fa51b9299fb3e9c43d781f4935
Fabiana Ribas <fabianalv.ribas@gmail.com>
2020-11-16 16:41:17 -0300

    Adicionado testes end-to-end da tela de pagamento e adicionado variaveis de ambiente ao arquivo de teste da pagina landing
    Signed-off-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- cypress/integration/landinglocalizador.spec.js
+++ cypress/integration/landinglocalizador.spec.js
@@ -1,34 +1,28 @@
 /// <reference types = "cypress"/>
 
-describe('LandingPage', () =>{
-  it('deve pesquisar o localizador', () =>{
-    cy.visit('http://localhost:3000')
-    cy.get('[name=InputSearch]').type("7P1PMX");
+describe('LandingPage', () => {
+  it('deve pesquisar o localizador', () => {
+    cy.visit(Cypress.env('BASE_URL'));
+    cy.get('.sc-bqyKva').type(Cypress.env('LOCATOR'));
 
     cy.server();
-    cy.route("GET", "**/locators").as("searchLocalizador")
+    cy.route('GET', '**/locators').as('searchLocalizador');
 
-    cy.get('.sc-crrsfI').click()
-    cy.url().should('include', '/debit_consultation')
-    cy.url().should('eq', 'http://localhost:3000/debit_consultation')
+    cy.get('.sc-crrsfI').click();
+    cy.url().should('include', Cypress.env('DEBITS_PATH'));
+    cy.url().should('eq', Cypress.env('DEBITS_URL'));
 
     // cy.wait('@searchLocalizador').then((xhr) =>{
     //   expect(xhr.status).be.eq(200);
     // })
-
   });
 
-  it('deve fazer pesquisa', () =>{
+  it('deve fazer pesquisa', () => {
     cy.request({
-      method:'GET',
-      url:'http://localhost:3000/debit_consultation'
-    }).then(response =>{
+      method: 'GET',
+      url: Cypress.env('DEBITS_URL')
+    }).then(response => {
       cy.log(response.body);
-    })
+    });
   });
-
-
 });
-
-
-
