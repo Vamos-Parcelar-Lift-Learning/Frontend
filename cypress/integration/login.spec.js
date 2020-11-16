c2995414b9a270c64524dd2688d2ed1a0215c0c6
Fabiana Ribas <fabianalv.ribas@gmail.com>
2020-11-16 17:34:03 -0300

    Adicionando correções aos testes da pagina de login
    Signed-off-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -4,12 +4,12 @@ describe('LogInPage', () => {
   it('realizar login', () => {
     cy.visit(Cypress.env('LOGIN_URL'));
     cy.url().should('include', Cypress.env('LOGIN_PATH'));
-    cy.get('.sc-biBrSq').contains('Digite seus dados de Login');
+    cy.get('.sc-jGVbCA').contains('Digite seus dados de Login');
 
-    cy.get('.sc-biBrSq').contains('Digite seus dados de Login');
-    cy.get(':nth-child(2) > .sc-hlTvYk').type(Cypress.env('LOGIN_USER'));
-    cy.get('.sc-biBrSq').contains('Digite seus dados de Login');
-    cy.get(':nth-child(4) > .sc-hlTvYk').type(Cypress.env('LOGIN_PW'));
+    cy.get('.sc-Fyfyc > :nth-child(1)').contains('Usuário');
+    cy.get(':nth-child(2) > .sc-hOqqkJ').type(Cypress.env('LOGIN_USER'));
+    cy.get('.sc-Fyfyc > :nth-child(3)').contains('Senha');
+    cy.get(':nth-child(4) > .sc-hOqqkJ').type(Cypress.env('LOGIN_PW'));
 
     cy.wait(5);
     cy.get('.MuiButtonBase-root').click();
@@ -24,18 +24,18 @@ describe('LogInPage', () => {
   it('Não realizar login', () => {
     cy.visit(Cypress.env('LOGIN_URL'));
     cy.url().should('include', Cypress.env('LOGIN_PATH'));
-    cy.get('.sc-biBrSq').contains('Digite seus dados de Login');
+    cy.get('.sc-jGVbCA').contains('Digite seus dados de Login');
 
-    cy.get('.sc-biBrSq').contains('Digite seus dados de Login');
-    cy.get(':nth-child(2) > .sc-hlTvYk').type(Cypress.env('LOGIN_USER_ERROR'));
-    cy.get('.sc-biBrSq').contains('Digite seus dados de Login');
-    cy.get(':nth-child(4) > .sc-hlTvYk').type(Cypress.env('LOGIN_PW_ERROR'));
+    cy.get('.sc-Fyfyc > :nth-child(1)').contains('Usuário');
+    cy.get(':nth-child(2) > .sc-hOqqkJ').type(Cypress.env('LOGIN_USER_ERROR'));
+    cy.get('.sc-Fyfyc > :nth-child(3)').contains('Senha');
+    cy.get(':nth-child(4) > .sc-hOqqkJ').type(Cypress.env('LOGIN_PW_ERROR'));
 
     cy.wait(5);
     cy.get('.MuiButtonBase-root').click();
 
     cy.wait(10);
-    //cy.get('.sc-gsTCUz').contains('Ops');
+    cy.get('.sc-gsTCUz').contains('Ops');
     cy.url().should('eq', Cypress.env('LOGIN_URL'));
   });
 });
