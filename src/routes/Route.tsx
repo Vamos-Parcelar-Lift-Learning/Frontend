1140ed9b95d10978914bcfb9eb0edeae54590004
Thiago Pereira <thiagorpereira7@gmail.com>
2020-11-17 15:20:27 -0300

    Configurando redirecionamentos

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -2,60 +2,58 @@ import React from 'react';
 import {
   RouteProps as RouteDOMProps,
   Route as RouteDOM,
-  Switch
-  // Redirect,
+  Switch,
+  Redirect
 } from 'react-router-dom';
 import { isMatch } from 'date-fns/esm';
 import DefaultLayout from '../pages/DefaultLayout';
-import Home from '../pages/Home';
-import Login from '../pages/Login'
+import { useAuth } from '../hooks/auth';
 
 interface RouteProps extends RouteDOMProps {
   isPrivate?: boolean;
   hasSidebar?: boolean;
+  isLogin?: boolean;
   component: React.ComponentType;
 }
 
 const Route: React.FC<RouteProps> = ({
-  // eslint-disable-next-line react/prop-types
   isPrivate = false,
   hasSidebar = false,
-  // eslint-disable-next-line react/prop-types
+  isLogin = false,
   component: Component,
   ...rest
 }) => {
   const Layout = DefaultLayout;
 
-  // const signed = false; // Pegar token de Login do Context API
-  // if (!signed && isPrivate) {
-  //   return <Redirect to="/" />;
-  // }
-  // if (signed && !isPrivate) {
-  //   return <Redirect to="/debit_consultation" />;
-  // }
+  const { user } = useAuth();
+
+  const signed = user;
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+  if (signed && !isPrivate && isLogin) {
+    return <Redirect to="/debit_consultation" />;
+  }
 
   return (
     <RouteDOM
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...rest}
       render={() => {
-        // return isPrivate && signed ? (
         return isPrivate ? (
           hasSidebar ? (
             <Layout>
               <Component />
             </Layout>
-          ):(
+          ) : (
             <Component />
           )
+        ) : hasSidebar ? (
+          <Layout>
+            <Component />
+          </Layout>
         ) : (
-          hasSidebar ? (
-            <Layout>
-              <Component />
-            </Layout>
-          ):(
-            <Component />
-          )
+          <Component />
         );
       }}
     />
