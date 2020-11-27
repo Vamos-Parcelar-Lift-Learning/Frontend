98b3445b8cdfe185cf4d51f87502387c3d5fa2a1
matheus rodrigues <matheus.1nascimento58@gmail.com>
2020-11-27 16:12:49 -0300

    adicionando modal de falha no pagamento

---
+++ src/components/PaymentFail/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  height: 80vh;
+  width: 40vw;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+export const PaymentImage = styled.img`
+  width: 80%;
+`;
+
+export const Info = styled.span`
+  font-size: 18px;
+  color: #4b4e5c;
+  font-weight: 500;
+  text-align: center;
+  line-height: 40px;
+  width: 85%;
+`;
+
+
+export const FailImage = styled.img`
+  width: 70px;
+  height: 70px;
+  position: absolute;
+  right: 30%;
+  top: 0;
+`;
