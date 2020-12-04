1e97993617202c4e132806d454f3ee955501f42d
Fabiana Ribas <fabianalv.ribas@gmail.com>
2020-12-04 18:59:53 -0300

    Arrumando posição do botão de imprimir e removendo botão de compartilhamento
    Signed-off-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- src/pages/Receipt/styles.ts
+++ src/pages/Receipt/styles.ts
@@ -50,6 +50,6 @@ export const FisrtButtonsContainer = styled.div`
   display: flex;
   width: 100%;
   margin-bottom: 10px;
-  justify-content: space-between;
+  justify-content: center;
   flex-direction: row;
 `;
