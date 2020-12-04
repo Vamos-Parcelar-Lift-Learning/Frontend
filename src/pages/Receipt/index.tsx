aee6aa6809ac871783aeb5fcdb7da6047746c381
Fabiana Ribas <fabianalv.ribas@gmail.com>
2020-12-04 18:56:41 -0300

    Adicionando comportamento de impressão no botão de imprimir comprovante
    Signed-off-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- src/pages/Receipt/index.tsx
+++ src/pages/Receipt/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PrintProvider, {Print, NoPrint} from "react-easy-print"
 import { Link } from 'react-router-dom';
 import { MdReceipt } from 'react-icons/md';
 import { useTranslation } from 'react-i18next';
@@ -19,35 +20,41 @@ import ButtonBlue from './ButtonBlue';
 
 const Receipt: React.FC = () => {
   const { t } = useTranslation();
+  const printContent = () => {
+    window.print();
+   };
   return (
     <Container>
       <Header />
-      <HeaderReceipt>
-        <MdReceipt color={colors.PRIMARY} size={45} title={t('receipt')} />
-        <Title>{t('receipt')}</Title>
-      </HeaderReceipt>
-      <FirstRowInfo>
-        <DataReceipt label={t('payday')} data="22/07/2020" />
-        <DataReceipt label={t('locator_code')} data="P7913F" />
-        <DataReceipt label={t('cashback')} data="R$5" />
-      </FirstRowInfo>
-      <SecondRowInfo>
-        <DataReceipt label={t('total_account')} data="R$ 120,00" />
-        <DataReceipt label={t('discount')} data="R$ 0" />
-        <DataReceipt label={t('total_service')} data="R$ 0" />
-        <DataReceipt label={t('total_payment')} data="R$ 120,00" />
-      </SecondRowInfo>
-
-      <ContainerButtons>
-        <FisrtButtonsContainer>
-          <ButtonWhite label={t('print')} />
-          <ButtonWhite label={t('share')} />
-        </FisrtButtonsContainer>
-
-        <Link to="/receipt">
-          <ButtonBlue label={t('return')} />
-        </Link>
-      </ContainerButtons>
+      <PrintProvider>
+        <Print single name='receipt'>
+          <HeaderReceipt>
+            <MdReceipt color={colors.PRIMARY} size={45} title={t('receipt')} />
+            <Title>{t('receipt')}</Title>
+          </HeaderReceipt>
+          <FirstRowInfo>
+            <DataReceipt label={t('payday')} data="22/07/2020" />
+            <DataReceipt label={t('locator_code')} data="P7913F" />
+            <DataReceipt label={t('cashback')} data="R$5" />
+          </FirstRowInfo>
+          <SecondRowInfo>
+            <DataReceipt label={t('total_account')} data="R$ 120,00" />
+            <DataReceipt label={t('discount')} data="R$ 0" />
+            <DataReceipt label={t('total_service')} data="R$ 0" />
+            <DataReceipt label={t('total_payment')} data="R$ 120,00" />
+          </SecondRowInfo>
+          <NoPrint>
+            <ContainerButtons>
+              <FisrtButtonsContainer>
+                <ButtonWhite onClick={() => printContent()} label={t('print')} />
+              </FisrtButtonsContainer>
+              <Link to="/receipt">
+                <ButtonBlue label={t('return')} />
+              </Link>
+            </ContainerButtons>
+          </NoPrint>
+        </Print>
+      </PrintProvider>
     </Container>
   );
 };
